c195bb02da0a563c0d361aa034ea0d09f18ca51f
Ketil Malde <ketil@malde.org>
2013-09-23 23:37:25 +0200

    add empty filtering to fclip
    Ignore-this: abd2e09c15fb6afdb07ac76a3a6a0d0d
    
    darcs-hash:c655f78ba0faf6e95fa5c650d1f710ca83d58849

--- src/FClip/Main.hs
+++ src/FClip/Main.hs
@@ -10,10 +10,11 @@ main = do
   when (null $ O.input o) $ error "Please provide an input file - or use --help for more information."
   when (null $ O.output o) $ error "Please provide an output file - or use --help for more information."  
   SFF h rs <- readSFF $ O.input o
-  let f = buildFilter o
-  writeSFF (O.output o) (SFF h $ map f rs) 
+  let t = buildTrim o
+      f = if O.filterEmpty o then filter (\r -> (num_bases (read_header r)) > 0) else id
+  writeSFF (O.output o) (SFF h $ f $ map t rs) 
 
-buildFilter :: O.Opts -> (ReadBlock -> ReadBlock)
-buildFilter o = if O.trimKey o then trimKey else id
+buildTrim :: O.Opts -> (ReadBlock -> ReadBlock)
+buildTrim o = if O.trimKey o then trimKey else id
               . if O.trim o then trim else id
               . if O.trimAdapter o then trimAdapter else id
--- src/FClip/Options.hs
+++ src/FClip/Options.hs
@@ -8,6 +8,7 @@ data Opts = Opts
             { trimKey :: Bool
             , trimAdapter :: Bool
             , trim    :: Bool
+            , filterEmpty :: Bool
             , output :: FilePath
             , input  :: FilePath
             } deriving (Data,Typeable, Show, Eq)
@@ -17,6 +18,7 @@ opts = Opts
   { trimKey = False &= help "Trim only the TCAG key sequence"             &= name "k"
   , trim    = False &= help "Trim quality using clipping information"     &= name "t"
   , trimAdapter = False &= help "Trim quality using adapter information"  &= name "a"
+  , filterEmpty = False &= help "Filter out reads that are empty after trimming" &= name "E"
   , output = def &= help "SFF file to write" &= name "o"
   , input  = def &= args &= typFile
   } 
